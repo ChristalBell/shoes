3c59be59dced45b18a2edc543ca3823513c58faf
Christal Bell <christalvictoriabell@gmail.com>
2023-10-27 22:02:34 -0700

    next and previous button work? inconsistent

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -2,13 +2,37 @@ import React from "react";
 import "./carousel.scss";
 
 const Carousel = () => {
+  const buttons = document.querySelectorAll("[data-carousel-button]");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+      const slides = button
+        .closest("[data-carousel]")
+        .querySelector("[data-slides]");
+
+      const activeSlide = slides.querySelector("[data-active]");
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+      if (newIndex < 0) newIndex = slides.children.length - 1;
+      if (newIndex >= slides.children.length) newIndex = 0;
+
+      slides.children[newIndex].dataset.active = true;
+      delete activeSlide.dataset.active;
+    });
+  });
   return (
     <div>
       <section>
-        <div className="carousel">
-          <button className="carousel-button previous">previous</button>
-          <button className="carousel-button next">next</button>
-          <ul>
+        <div className="carousel" data-carousel>
+          <button
+            className="carousel-button previous"
+            data-carousel-button="previous"
+          >
+            previous
+          </button>
+          <button className="carousel-button next" data-carousel-button="next">
+            next
+          </button>
+          <ul data-slides>
             <li className="slide" data-active>
               <img alt="first" src="./images/image-product-1.jpg " />
             </li>
