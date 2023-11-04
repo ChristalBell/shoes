2520e952d0b445823a368a76cf053ee77b466900
Christal Bell <christalvictoriabell@gmail.com>
2023-11-04 15:29:46 -0700

    counter in add to cart working finally

--- src/components/shoe detail/AddToCart.js
+++ src/components/shoe detail/AddToCart.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux/es/hooks/useSelector";
-import { selectCount } from "../../app/counterReducer";
+// import { selectCount } from "../../app/counterReducer";
+import { useDispatch } from "react-redux";
+import { added, subtracted } from "../../app/counterReducer";
 
 const AddToCart = () => {
   const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div>
       <div className="flex">
-        <button> - </button>
+        <button onClick={() => dispatch(subtracted())}> - </button>
         <div> {count} </div>
-        <button> + </button>
+        <button onClick={() => dispatch(added())}> + </button>
       </div>
       <button> Add to Cart </button>
     </div>
