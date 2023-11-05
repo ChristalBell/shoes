4b52e87bc72aed968ee4b232c9e4aac638ef7cc5
Christal Bell <christalvictoriabell@gmail.com>
2023-11-04 23:22:27 -0700

    mobile carousel complete 1/2 desktop

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -10,16 +10,65 @@ const Carousel = () => {
     "./images/image-product-4.jpg",
   ];
 
+  const [mainPic, setMainPic] = useState("./images/image-product-1.jpg");
+
   return (
     //mobile
-    <div className="carousel">
-      <button id="prev" onClick={() => setCurrentPic(currentPic - 1)}>
-        PREV
-      </button>
-      <img src={pics[currentPic]} alt="sneaker pics" />
-      <button id="next" onClick={() => setCurrentPic(currentPic + 1)}>
-        NEXT
-      </button>
+    <div>
+      <div className="carousel">
+        <button id="prev" onClick={() => setCurrentPic(currentPic - 1)}>
+          PREV
+        </button>
+        <img src={pics[currentPic]} alt="sneaker pics" />
+        <button id="next" onClick={() => setCurrentPic(currentPic + 1)}>
+          NEXT
+        </button>
+      </div>
+
+      {/* //desktop */}
+      <div className="slide-pic">
+        <img
+          id="main"
+          src={mainPic}
+          alt="selected shoe view"
+          height={400}
+          width={400}
+        />
+
+        <div className="thumbnails ">
+          <button onClick={() => setMainPic("./images/image-product-1.jpg")}>
+            {" "}
+            <img
+              src="./images/image-product-1-thumbnail.jpg"
+              alt="bottom and side shoe view"
+            />
+          </button>
+
+          <button onClick={() => setMainPic("./images/image-product-2.jpg")}>
+            {" "}
+            <img
+              src="./images/image-product-2-thumbnail.jpg"
+              alt="side on top of rock and side shoe view"
+            />
+          </button>
+
+          <button onClick={() => setMainPic("./images/image-product-3.jpg")}>
+            {" "}
+            <img
+              src="./images/image-product-3-thumbnail.jpg"
+              alt="one shoe on rock side shoe view"
+            />
+          </button>
+
+          <button onClick={() => setMainPic("./images/image-product-4.jpg")}>
+            {" "}
+            <img
+              src="./images/image-product-4-thumbnail.jpg"
+              alt="one shoe on rock other side shoe view"
+            />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
