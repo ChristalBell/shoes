48d1c2d440fc8f82b6bf8a4f901d42907a235d22
Christal Bell <christalvictoriabell@gmail.com>
2023-11-04 20:20:48 -0700

    minor header fixes and mobile carousel added

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,89 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./carousel.scss";
 
 const Carousel = () => {
-  const buttons = document.querySelectorAll("[data-carousel-button]");
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-      const slides = button
-        .closest("[data-carousel]")
-        .querySelector("[data-slides]");
+  const [currentPic, setCurrentPic] = useState(0);
+  const pics = [
+    "./images/image-product-1.jpg",
+    "./images/image-product-2.jpg",
+    "./images/image-product-3.jpg",
+    "./images/image-product-4.jpg",
+  ];
 
-      const activeSlide = slides.querySelector("[data-active]");
-      let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-      if (newIndex < 0) newIndex = slides.children.length - 1;
-      if (newIndex >= slides.children.length) newIndex = 0;
-
-      slides.children[newIndex].dataset.active = true;
-      delete activeSlide.dataset.active;
-    });
-  });
   return (
-    <div>
-      <section>
-        <div className="carousel" data-carousel>
-          <button
-            className="carousel-button previous"
-            data-carousel-button="previous"
-          >
-            previous
-          </button>
-
-          <button className="carousel-button next" data-carousel-button="next">
-            next
-          </button>
-
-          <ul data-slides>
-            <li className="slide" data-active>
-              <img alt="first" src="./images/image-product-1.jpg " />
-            </li>
-            <li className="slide">
-              <img alt="second" src="./images/image-product-2.jpg" />
-            </li>
-            <li className="slide">
-              <img alt="third" src="./images/image-product-3.jpg" />
-            </li>
-            <li className="slide">
-              <img alt="fourth" src="./images/image-product-4.jpg" />
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      {/* // desktop version */}
-      <div className="slide-pic">
-        <div>
-          <img className="main" src="./images/image-product-1.jpg" alt="main" />
-          <div className="overlay"></div>
-        </div>
-
-        <div className="thumb-bar">
-          <img
-            className="pic1 thumbnail"
-            src="./images/image-product-1-thumbnail.jpg"
-            alt="pic1"
-          />
-
-          <img
-            className="pic2 thumbnail"
-            src="./images/image-product-2-thumbnail.jpg"
-            alt="pic2"
-          />
-
-          <img
-            className="pic3 thumbnail"
-            src="./images/image-product-3-thumbnail.jpg"
-            alt="pic3"
-          />
-
-          <img
-            className="pic4 thumbnail"
-            src="./images/image-product-4-thumbnail.jpg"
-            alt="pic4"
-          />
-        </div>
-      </div>
+    //mobile
+    <div className="carousel">
+      <button id="prev" onClick={() => setCurrentPic(currentPic - 1)}>
+        PREV
+      </button>
+      <img src={pics[currentPic]} alt="sneaker pics" />
+      <button id="next" onClick={() => setCurrentPic(currentPic + 1)}>
+        NEXT
+      </button>
     </div>
   );
 };
